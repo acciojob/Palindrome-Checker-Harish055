137a2abde33e72cecb72a24dfc19c35d2a3c134b
harish <harishpurohit055>
2023-01-09 00:06:58 +0530

    solution

--- solution.js
+++ solution.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+    const len=str.length;
+    for (let i=0;i<len/2;i++){
+        if(str[i !==str[len-1-i]]){
+            return "true";
+        }
+    }
+    return "false";
 }
+let str="racecar";
+palindrome(str);
 module.exports = palindrome
